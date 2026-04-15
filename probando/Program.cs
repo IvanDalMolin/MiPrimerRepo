debd88a78979a61db9b920b7f161e752108448b7
Ragnar <ivan.boca98@gmail.com>
2026-04-15 06:50:55 +0000

    cadena nueva

--- probando/Program.cs
+++ probando/Program.cs
@@ -2,3 +2,10 @@
 Console.WriteLine("Hello, lindos!");
 
 string stripe_api_key="sk_test_4edsgsdafkjgjdasfkglsjk"
+
+https://github.com/settings/personal-access-tokens/new
+  ?name=Repo-reading+token
+  &description=Just+contents:read
+  &target_name=octodemo
+  &expires_in=45
+  &contents=read
